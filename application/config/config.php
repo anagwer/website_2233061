71acb4aaba14a93f672ac5894a40c2ebac31bc07
Giwang <ira.nuryani@lidahbuayagroup.com>
2025-07-14 11:54:12 +0700

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/project/website_2233061/';
-// $config['base_url'] = 'http://localhost/PAB/website_2233061/';
+// $config['base_url'] = 'http://localhost/project/website_2233061/';
+$config['base_url'] = 'http://localhost/PAB/website_2233061/';
 
 /*
 |--------------------------------------------------------------------------
